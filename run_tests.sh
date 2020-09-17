0a51dba73d2ced25657c355570165b3a04863ba4
pabra <patrick+github-commit-2020@braune.xyz>
2020-09-17 17:46:24 +0200

    cleanup and no tty for test

--- run_tests.sh
+++ run_tests.sh
@@ -8,13 +8,19 @@ cleanup() {
 
 trap cleanup EXIT INT
 
+if [ -t 1 ]; then
+    no_tty=
+else
+    no_tty='-T'
+fi
+
 if [ "$1" != 'skip_build' ]; then
     docker-compose -f test/docker-compose.yml build
 fi
 
 docker-compose -f test/docker-compose.yml up -d
-docker-compose -f test/docker-compose.yml run awstats-test wait-for-it.sh awstats:80 -t 10 -- test1.sh
-docker-compose -f test/docker-compose.yml exec awstats awstats_updateall.pl now
-docker-compose -f test/docker-compose.yml run awstats-test test2.sh
-docker-compose -f test/docker-compose.yml exec awstats awstats_updateall.pl now
-docker-compose -f test/docker-compose.yml run awstats-test test3.sh
+docker-compose -f test/docker-compose.yml run ${no_tty} awstats-test wait-for-it.sh awstats:80 -t 10 -- test1.sh
+docker-compose -f test/docker-compose.yml exec ${no_tty} awstats awstats_updateall.pl now
+docker-compose -f test/docker-compose.yml run ${no_tty} awstats-test test2.sh
+docker-compose -f test/docker-compose.yml exec ${no_tty} awstats awstats_updateall.pl now
+docker-compose -f test/docker-compose.yml run ${no_tty} awstats-test test3.sh
