5a9d07e731e6bcdc7db3900ce57a137132003f1d
pabra <patrick+github-commit-2021@braune.xyz>
2021-03-21 21:04:35 +0100

    formatting
    # Conflicts:
    #	test/test1.sh
    #	test/test2.sh
    #	test/test3.sh

--- test/test1.sh
+++ test/test1.sh
@@ -3,17 +3,17 @@
 set -ex
 
 # index page should show expected frameset
-diff -u html.index.txt <( lynx -dump 'http://awstats:80' )
+diff -u html.index.txt <(lynx -dump 'http://awstats:80')
 
 # check details page
 diff -u -I 'Last Update:' \
     html.02-2020_empty.txt \
-    <( lynx -dump 'http://awstats:80/awstats.pl?month=02&year=2020&output=main&framename=mainright' )
+    <(lynx -dump 'http://awstats:80/awstats.pl?month=02&year=2020&output=main&framename=mainright')
 
 # /var/lib/awstats should be empty
 diff -u \
-    <( ls -1AF /var/lib/awstats 2>&1 ) \
-    <( echo -n '' )
+    <(ls -1AF /var/lib/awstats 2>&1) \
+    <(echo -n '')
 
 cp access.log /var/local/log
 
--- test/test2.sh
+++ test/test2.sh
@@ -3,17 +3,17 @@
 set -ex
 
 # index page should show expected frameset
-diff -u html.index.txt <( lynx -dump 'http://awstats:80' )
+diff -u html.index.txt <(lynx -dump 'http://awstats:80')
 
 # check details page
 diff -u -I 'Last Update:' \
     html.02-2020_with_entries.txt \
-    <( lynx -dump 'http://awstats:80/awstats.pl?month=02&year=2020&output=main&framename=mainright' )
+    <(lynx -dump 'http://awstats:80/awstats.pl?month=02&year=2020&output=main&framename=mainright')
 
 # /var/lib/awstats should contain 1 file
 diff -u \
-    <( ls -1AF /var/lib/awstats 2>&1 ) \
-    <( echo 'awstats022020.txt' )
+    <(ls -1AF /var/lib/awstats 2>&1) \
+    <(echo 'awstats022020.txt')
 
 unlink /var/lib/awstats/awstats022020.txt
 
@@ -22,6 +22,6 @@ cp awstats022020.txt /var/lib/awstats/
 # check details page
 diff -u -I 'Last Update:' \
     html.02-2020_old_entries.txt \
-    <( lynx -dump 'http://awstats:80/awstats.pl?month=02&year=2020&output=main&framename=mainright' )
+    <(lynx -dump 'http://awstats:80/awstats.pl?month=02&year=2020&output=main&framename=mainright')
 
 echo 'tests 2 done'
