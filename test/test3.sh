e6147bd612019bc5a9ed153632c4423f71c4f769
pabra <patrick+github-commit-2023@braune.xyz>
2023-12-14 21:52:29 +0100

    run tests on port 80 and 4567

--- test/test3.sh
+++ test/test3.sh
@@ -2,19 +2,25 @@
 
 set -ex
 
+PORT="$1"
+
+if [ -z "$PORT" ]; then
+    PORT=80
+fi
+
 # index page should show expected frameset
 diff -u \
-    html.index.txt \
-    <(lynx -dump 'http://awstats:80')
+    "html.${PORT}.index.txt" \
+    <(lynx -dump "http://awstats:${PORT}")
 
 # check details page
 diff -u -I 'Last Update:' \
-    html.02-2020_old_and_new_entries.txt \
-    <(lynx -dump 'http://awstats:80/awstats.pl?databasebreak=month&month=02&year=2020&output=main&framename=mainright')
+    "html.${PORT}.02-2020_old_and_new_entries.txt" \
+    <(lynx -dump "http://awstats:${PORT}/awstats.pl?databasebreak=month&month=02&year=2020&output=main&framename=mainright")
 
 # /var/lib/awstats should contain 1 file
 diff -u \
     <(ls -1AF /var/lib/awstats 2>&1) \
     <(echo 'awstats022020.txt')
 
-echo 'tests 3 done'
+echo "test 3 on port ${PORT} done"
