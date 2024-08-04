bceba32d5dc7dc08938665a1a9f0ef07de4760de
pabra <patrick+github-commit-2024@braune.xyz>
2024-08-04 11:23:12 +0200

    publish: use `docker compose` over `docker-compose`

--- run_tests.sh
+++ run_tests.sh
@@ -3,7 +3,7 @@
 set -e
 
 cleanup() {
-    docker-compose -f test/docker-compose.yml down --volumes
+    docker compose -f test/docker-compose.yml down --volumes
 }
 
 trap cleanup EXIT INT
@@ -17,17 +17,17 @@ fi
 run_tests() {
     export HTTPD_PORT="$1"
     export USER="$2"
-    docker-compose -f test/docker-compose.yml up -d
-    docker-compose -f test/docker-compose.yml run --rm ${no_tty} awstats-test /bin/sh -c "wait4ports -t 10 tcp://awstats:$HTTPD_PORT"
-    docker-compose -f test/docker-compose.yml run --rm ${no_tty} awstats-test test1.sh "$HTTPD_PORT"
-    docker-compose -f test/docker-compose.yml exec ${no_tty} awstats awstats_updateall.pl now
-    docker-compose -f test/docker-compose.yml run --rm ${no_tty} awstats-test test2.sh "$HTTPD_PORT"
-    docker-compose -f test/docker-compose.yml exec ${no_tty} awstats awstats_updateall.pl now
-    docker-compose -f test/docker-compose.yml run --rm ${no_tty} awstats-test test3.sh "$HTTPD_PORT"
+    docker compose -f test/docker-compose.yml up -d
+    docker compose -f test/docker-compose.yml run --rm ${no_tty} awstats-test /bin/sh -c "wait4ports -t 10 tcp://awstats:$HTTPD_PORT"
+    docker compose -f test/docker-compose.yml run --rm ${no_tty} awstats-test test1.sh "$HTTPD_PORT"
+    docker compose -f test/docker-compose.yml exec ${no_tty} awstats awstats_updateall.pl now
+    docker compose -f test/docker-compose.yml run --rm ${no_tty} awstats-test test2.sh "$HTTPD_PORT"
+    docker compose -f test/docker-compose.yml exec ${no_tty} awstats awstats_updateall.pl now
+    docker compose -f test/docker-compose.yml run --rm ${no_tty} awstats-test test3.sh "$HTTPD_PORT"
 }
 
 if [ "$1" != 'skip_build' ]; then
-    docker-compose -f test/docker-compose.yml build
+    docker compose -f test/docker-compose.yml build
 fi
 
 run_tests 80 0
